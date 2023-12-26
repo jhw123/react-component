4a12ba2e5f58ff53ed837cd3830fb98a43118884
Hyoungwook Jin <hyoungwookjin123@gmail.com>
2023-12-26 12:42:39 +0900

    Update package.json version and import paths

--- src/components/utils/sheet.tsx
+++ src/components/utils/sheet.tsx
@@ -1,9 +1,7 @@
 import styled from '@emotion/styled'
 import { css } from '@emotion/react'
 import React from 'react'
-import { Fill } from '@src/foundation/fill'
-import { View } from '@src/foundation'
-import { Elevation } from '@src/foundation/elevation'
+import { Fill, View, Elevation } from '../../foundation'
 
 interface Props {
   fill: Fill
