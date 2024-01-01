aeee3cd8c25cfee2291f3d0b3277a200f2638747
Hyoungwook Jin <hyoungwookjin123@gmail.com>
2024-01-02 00:10:16 +0900

    💄 update text input auto height logic

--- src/components/inputs/text.stories.tsx
+++ src/components/inputs/text.stories.tsx
@@ -2,7 +2,13 @@ import { Global } from '@emotion/react'
 import type { Meta, StoryObj } from '@storybook/react'
 import React from 'react'
 import { TextInput } from '..'
-import { ResetStyle } from '../../foundation'
+import { Elevation, ResetStyle } from '../../foundation'
+import styled from '@emotion/styled'
+
+const Window = styled.div`
+  width: 500px;
+  ${Elevation.L1}
+`
 
 const meta = {
   title: 'Component/Input/Text',
@@ -14,10 +20,10 @@ const meta = {
   argTypes: {},
   decorators: [
     Story => (
-      <>
+      <Window>
         <Global styles={ResetStyle} />
         <Story />
-      </>
+      </Window>
     ),
   ],
 } satisfies Meta<typeof TextInput>
--- src/components/inputs/text.tsx
+++ src/components/inputs/text.tsx
@@ -1,20 +1,20 @@
 import styled from '@emotion/styled'
 import React, { ChangeEvent, KeyboardEvent, useCallback } from 'react'
-import { MIN_BUTTON_SIZE } from '../../foundation/size'
 import { Border, Color, ComponentState, Fill, Font, View } from '../../foundation'
+import { css } from '@emotion/react'
 
 interface Props {
   value: string
   state?: Exclude<ComponentState, 'Checked'>
   placeholder?: string
-  rows?: number
+  maxRows?: number
   readonly?: boolean
   onChange?: (value: string) => void
   onEnter?: () => void
 }
 
 export const TextInput = View<Props>(
-  ({ value, onChange, state = 'Default', placeholder, rows = 3, readonly = false, onEnter, ...props }) => {
+  ({ value, onChange, state = 'Default', placeholder, maxRows = 0, readonly = false, onEnter, ...props }) => {
     const onWrite = useCallback(
       (e: ChangeEvent<HTMLTextAreaElement>) => {
         onChange?.(e.target.value)
@@ -33,38 +33,56 @@ export const TextInput = View<Props>(
     )
 
     return (
-      <Container
-        {...props}
-        onChange={onWrite}
-        value={value}
-        disabled={state === 'Disabled'}
-        placeholder={placeholder}
-        rows={rows}
-        readOnly={readonly}
-        onKeyUp={onKeyDown}
-      />
+      <Container {...props} rows={maxRows}>
+        <HeightResizer>{value + '\n'}</HeightResizer>
+        <InputBox
+          onChange={onWrite}
+          value={value}
+          disabled={state === 'Disabled'}
+          placeholder={placeholder}
+          readOnly={readonly}
+          onKeyUp={onKeyDown}
+        />
+      </Container>
     )
   }
 )
 
-const Container = styled.textarea`
-  width: 100%;
+const Container = styled.div<{ rows: number }>`
+  ${({ rows }) => css`
+    position: relative;
+    width: 100%;
+    border-radius: 8px;
+    ${Border.Secondary}
+    padding: 8px;
+    ${Fill.Secondary}
+    height: fit-content;
+    ${0 < rows &&
+    css`
+      max-height: ${rows * 1.4}rem;
+    `}
+    overflow: auto;
+
+    &:focus-within {
+      ${Color.Primary}
+      ${Border.Focus}
+    }
+  `}
+`
+
+const InputBox = styled.textarea`
+  width: calc(100% - 16px);
   resize: vertical;
   box-sizing: border-box;
   font-family: inherit;
   ${Font.Body}
-  border-radius: 8px;
-  ${Border.Secondary}
-  padding: 8px;
-  line-height: inherit;
-  min-height: ${MIN_BUTTON_SIZE}px;
-  ${Fill.Secondary}
   outline: none;
-
-  &:focus {
-    ${Color.Primary}
-    ${Border.Focus}
-  }
+  position: absolute;
+  height: calc(100% - 16px);
+  resize: none;
+  overflow: hidden;
+  top: 8px;
+  left: 8px;
 
   ::placeholder {
     ${Color.Secondary}
@@ -74,4 +92,10 @@ const Container = styled.textarea`
     cursor: not-allowed;
     filter: grayscale(1);
   }
+`
+
+const HeightResizer = styled.div`
+  white-space: pre-line;
+  ${Font.Body}
+  opacity: 0;
 `
