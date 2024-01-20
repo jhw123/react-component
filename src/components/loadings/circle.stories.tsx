9551a8dc67ef8a43cf5d3ca25443112243f0107e
Hyoungwook Jin <hyoungwookjin123@gmail.com>
2024-01-20 16:48:14 +0900

    ✨ add circle loading

---
+++ src/components/loadings/circle.stories.tsx
@@ -0,0 +1,62 @@
+import { Global } from '@emotion/react'
+import type { Meta, StoryObj } from '@storybook/react'
+import React from 'react'
+import { CircleLoading } from '..'
+import { ResetStyle } from '../../foundation'
+
+const meta = {
+  title: 'Component/Loading/Circle',
+  component: CircleLoading,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+  argTypes: {},
+  decorators: [
+    Story => (
+      <>
+        <Global styles={ResetStyle} />
+        <Story />
+      </>
+    ),
+  ],
+} satisfies Meta<typeof CircleLoading>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Primary: Story = {
+  args: {
+    border: 'Primary',
+  },
+}
+
+export const Secondary: Story = {
+  args: {
+    border: 'Secondary',
+  },
+}
+
+export const Success: Story = {
+  args: {
+    border: 'Success',
+  },
+}
+
+export const Danger: Story = {
+  args: {
+    border: 'Danger',
+  },
+}
+
+export const Warning: Story = {
+  args: {
+    border: 'Warning',
+  },
+}
+
+export const Focus: Story = {
+  args: {
+    border: 'Focus',
+  },
+}
---
+++ src/components/loadings/circle.tsx
@@ -0,0 +1,49 @@
+import { css, keyframes } from '@emotion/react'
+import styled from '@emotion/styled'
+import React from 'react'
+import { Border } from '../../foundation'
+
+interface Props {
+  border?: Border
+}
+
+export const CircleLoading = ({ border = 'Focus' }: Props) => {
+  return <Container border={border} />
+}
+
+const spinning = keyframes`
+  100%
+  {
+    transform: rotate(360deg);
+  }
+`
+
+const Container = styled.div<{ border: Border }>`
+  ${({ border }) => css`
+    width: 1.5rem;
+    height: 1.5rem;
+    box-sizing: border-box;
+    ${Border[border]}
+    border-left-color: transparent;
+    border-right-color: transparent;
+    border-bottom-color: transparent;
+    border-radius: 100%;
+    position: relative;
+    animation: ${spinning} 0.8s ease-in-out infinite;
+
+    &:before {
+      content: '';
+      width: calc(100% + 4px);
+      height: calc(100% + 4px);
+      box-sizing: border-box;
+      ${Border[border]}
+      opacity: 0.3;
+      border-radius: 100%;
+      display: block;
+      position: absolute;
+      left: 50%;
+      top: 50%;
+      transform: translate(-50%, -50%);
+    }
+  `}
+`
