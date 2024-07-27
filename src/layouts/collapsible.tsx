423109aa6a68ce14b1b19ec157da66f4c2060d42
jhw123 <hyoungwookjin123@gmail.com>
2024-07-28 08:50:14 +0900

    🐛 skip the first transition of collapsible

--- src/layouts/collapsible.tsx
+++ src/layouts/collapsible.tsx
@@ -1,6 +1,7 @@
 import { css } from '@emotion/react'
 import styled from '@emotion/styled'
-import React from 'react'
+import React, { useState } from 'react'
+import { useEffectOnce } from 'react-use'
 import { View } from '../foundation'
 
 interface Props {
@@ -8,18 +9,31 @@ interface Props {
   children: React.ReactNode
 }
 
+const TRANSITION_DURATION = 500
+
 export const CollapsibleLayout = View<Props>(({ children, checked = true, ...props }) => {
+  const [transitionOn, setTransitionOn] = useState(false)
+
+  useEffectOnce(() => {
+    setTimeout(() => {
+      setTransitionOn(true)
+    }, TRANSITION_DURATION + 1)
+  })
+
   return (
-    <Container {...props} collapsed={!checked}>
+    <Container {...props} collapsed={!checked} transitionOn={transitionOn}>
       {children}
     </Container>
   )
 })
 
-const Container = styled.div<{ collapsed: boolean }>`
-  ${({ collapsed }) => css`
+const Container = styled.div<{ collapsed: boolean; transitionOn: boolean }>`
+  ${({ collapsed, transitionOn }) => css`
     max-height: ${collapsed ? 0 : '2000px'};
     overflow: hidden;
-    transition: max-height 0.5s ease-in-out;
+    ${transitionOn &&
+    css`
+      transition: max-height ${TRANSITION_DURATION}ms ease-in-out;
+    `}
   `}
 `
