38f57e373778d0e2ad44bfe56d67879978895a6c
jhw123 <hyoungwookjin123@gmail.com>
2024-07-28 09:42:29 +0900

    ✨ add fill attribute to tabLayout

--- src/layouts/tab.tsx
+++ src/layouts/tab.tsx
@@ -2,39 +2,43 @@ import { css } from '@emotion/react'
 import styled from '@emotion/styled'
 import React, { useCallback } from 'react'
 import { View } from '../foundation'
+import { Fill } from '../themes/default/fill'
 
 interface Props {
   tabTitles: string[]
   tabIcons?: string[]
   children: React.ReactNode
+  fill?: Fill
   onTabChange?: (index: number) => void
 }
 
-export const TabLayout = View<Props>(({ children, forwardedRef, tabIcons, tabTitles, onTabChange, ...props }) => {
-  const [selected, setSelected] = React.useState(0)
+export const TabLayout = View<Props>(
+  ({ children, forwardedRef, tabIcons, tabTitles, fill = 'Brand', onTabChange, ...props }) => {
+    const [selected, setSelected] = React.useState(0)
 
-  const handleTabChange = useCallback(
-    (index: number) => () => {
-      setSelected(index)
-      onTabChange?.(index)
-    },
-    [onTabChange]
-  )
+    const handleTabChange = useCallback(
+      (index: number) => () => {
+        setSelected(index)
+        onTabChange?.(index)
+      },
+      [onTabChange]
+    )
 
-  return (
-    <Container {...props}>
-      <TabContainer>
-        {tabTitles.map((title, i) => (
-          <Tab key={i} onClick={handleTabChange(i)} active={selected === i}>
-            {tabIcons && <img src={tabIcons[i]} width={24} height={24} alt={title} />}
-            {title}
-          </Tab>
-        ))}
-      </TabContainer>
-      {React.Children.map(children, (child, i) => selected === i && child)}
-    </Container>
-  )
-})
+    return (
+      <Container {...props}>
+        <TabContainer>
+          {tabTitles.map((title, i) => (
+            <Tab key={i} onClick={handleTabChange(i)} fill={fill} active={selected === i}>
+              {tabIcons && <img src={tabIcons[i]} width={24} height={24} alt={title} />}
+              {title}
+            </Tab>
+          ))}
+        </TabContainer>
+        {React.Children.map(children, (child, i) => selected === i && child)}
+      </Container>
+    )
+  }
+)
 
 const Container = styled.div`
   height: 100%;
@@ -55,8 +59,8 @@ const TabContainer = styled.div`
   `}
 `
 
-const Tab = styled.button<{ active: boolean }>`
-  ${({ theme, active }) => css`
+const Tab = styled.button<{ active: boolean; fill: Fill }>`
+  ${({ theme, active, fill }) => css`
     padding: 8px;
     border-bottom: none;
     position: relative;
@@ -79,7 +83,7 @@ const Tab = styled.button<{ active: boolean }>`
         left: 0;
         height: 4px;
         width: 100%;
-        ${theme.fill.Brand};
+        ${theme.fill[fill]};
       }
     `}
   `}
