c671ac7a98db3d24cc5db41ed28c0419ab8ef53f
Hyoungwook Jin <hyoungwookjin123@gmail.com>
2024-10-24 01:39:18 +0900

    💄 update styling of progressLayout

--- src/layouts/progress.tsx
+++ src/layouts/progress.tsx
@@ -3,20 +3,23 @@ import styled from '@emotion/styled'
 import React from 'react'
 import { Children } from 'react'
 import { View } from '../foundation'
+import { Fill } from '../themes/default/fill'
 
 interface Props {
   children?: React.ReactNode
+  type?: Fill
   stage: number
 }
 
-export const ProgressLayout = View<Props>(({ children, stage, ...props }) => {
+export const ProgressLayout = View<Props>(({ children, stage, type = 'Focus', forwardedRef, ...props }) => {
   return (
     <Container {...props}>
       {Children.map(children, (child, i) => {
+        const staus = i < stage ? 'Complete' : i === stage ? 'InProgress' : 'NotComplete'
         return (
-          <Stage key={i}>
+          <Stage key={i} fill={type}>
             <Header>
-              <Status status={i < stage ? 'Complete' : 'InProgress'} />
+              <Status fill={type} status={staus} />
               {child}
             </Header>
           </Stage>
@@ -38,11 +41,11 @@ const Header = styled.div`
   justify-content: start;
 `
 
-const Stage = styled.div`
-  ${({ theme }) => css`
+const Stage = styled.div<{ fill: Fill }>`
+  ${({ theme, fill }) => css`
     &:not(:last-of-type) {
       padding-bottom: 16px;
-      ${theme.border.Focus}
+      ${theme.border[fill]}
       border-width: 0;
       border-left-width: 2px;
     }
@@ -52,30 +55,44 @@ const Stage = styled.div`
   `}
 `
 
-const Status = styled.div<{ status: 'Complete' | 'InProgress' }>`
-  ${({ theme, status }) => css`
+const Status = styled.div<{ fill: Fill; status: 'Complete' | 'InProgress' | 'NotComplete' }>`
+  ${({ theme, status, fill }) => css`
     width: 24px;
     height: 24px;
     border-radius: 50%;
-    ${theme.border.Focus}
-    ${status === 'Complete' ? theme.fill.Focus : theme.fill.Primary}
+    ${theme.border[fill]}
+    ${status === 'Complete' ? theme.fill[fill] : theme.fill.Primary}
     display: flex;
     justify-content: center;
     align-items: center;
-    ${theme.color.Focus}
+    ${theme.color[fill]}
     ${status === 'Complete' && 'font-size: 0;'}
     line-height: normal;
     transform: translateX(-13px);
+    position: relative;
+
+    ::before {
+      content: '';
+      width: 12px;
+      height: 12px;
+      border-radius: 50%;
+      ${theme.fill[fill]}
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      display: ${status === 'InProgress' ? 'block' : 'none'};
+    }
 
     ::after {
       content: '';
-      border: white 1px solid;
+      ${theme.border.Contrast}
       width: 12px;
       height: 6px;
       border-width: 0 0 2px 2px;
       position: absolute;
-      top: 2px;
-      left: 2px;
+      top: 0px;
+      left: 0px;
       transform: translate(3px, 4px) rotate(-45deg);
       display: ${status === 'Complete' ? 'block' : 'none'};
     }
