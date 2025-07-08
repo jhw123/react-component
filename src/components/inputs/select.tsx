874b3d15f6dc1fbab0d055c0271fb33cf16bf380
Hyoungwook Jin <hyoungwookjin123@gmail.com>
2025-07-08 14:57:26 +0900

    ✨ add children and border props to SelectInput

--- src/components/inputs/select.tsx
+++ src/components/inputs/select.tsx
@@ -3,52 +3,61 @@ import React, { ChangeEvent, useCallback } from 'react'
 import { View } from '../../foundation'
 import { MIN_BUTTON_SIZE } from '../../foundation/size'
 import { css } from '@emotion/react'
+import { Border } from '../../themes/default/border'
 
 interface Props {
+  children?: React.ReactNode
   options: Readonly<string[]>
   value?: string
+  border?: Border
   onSelect: (index: number, value: string) => void
 }
 
-export const SelectInput = View<Props>(({ options, onSelect, value, forwardedRef, ...props }) => {
-  const onClick = useCallback(
-    (e: ChangeEvent<HTMLSelectElement>) => {
-      const i = options.findIndex(op => op === e.target.value)
-      onSelect(i, options[i])
-    },
-    [onSelect, options]
-  )
+export const SelectInput = View<Props>(
+  ({ options, border = 'Secondary', onSelect, value, forwardedRef, children, ...props }) => {
+    const onClick = useCallback(
+      (e: ChangeEvent<HTMLSelectElement>) => {
+        const i = options.findIndex(op => op === e.target.value)
+        onSelect(i, options[i])
+      },
+      [onSelect, options]
+    )
 
-  return (
-    <Container {...props}>
-      <Options onChange={onClick} value={value} ref={forwardedRef}>
-        {options.map((option, i) => (
-          <option key={i}>{option}</option>
-        ))}
-      </Options>
-      <ArrowDown />
-    </Container>
-  )
-})
+    return (
+      <Container {...props} border={border}>
+        {children}
+        <Options onChange={onClick} value={value} ref={forwardedRef}>
+          {options.map((option, i) => (
+            <option key={i}>{option}</option>
+          ))}
+        </Options>
+        <ArrowDown />
+      </Container>
+    )
+  }
+)
 
-const Container = styled.div`
-  position: relative;
+const Container = styled.div<{ border: Border }>`
+  ${({ theme, border }) => css`
+    ${theme.border[border]}
+    border-radius: 8px;
+    position: relative;
+    display: flex;
+    align-items: center;
+    gap: 4px;
+  `}
 `
 
 const Options = styled.select`
-  ${({ theme }) => css`
-    padding: 4px;
-    padding-right: 16px;
-    border-radius: 8px;
-    height: ${MIN_BUTTON_SIZE}px;
-    cursor: pointer;
-    ${theme.border.Secondary}
-    outline: none;
-    width: max-content;
-    font-size: inherit;
-    text-align: center;
-    appearance: none;
-  `}
+  padding: 4px;
+  padding-right: 20px;
+  height: ${MIN_BUTTON_SIZE}px;
+  cursor: pointer;
+  outline: none;
+  width: max-content;
+  font-size: inherit;
+  text-align: center;
+  appearance: none;
 `
 
 const ArrowDown = styled.div`
